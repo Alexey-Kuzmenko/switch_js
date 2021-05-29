ccd5f7009a6cce200a367ee6b6e950b9e08b56c0
Alexey Kuzmenko <alesha1103348@gmail.com>
2021-05-29 18:14:20 +0300

    second exercise

--- switch.js
+++ switch.js
@@ -16,48 +16,34 @@
 //         break;
 // }
 
-//* exercise 1
+//* exercise 2
 
-//TODO: rewrote with if construction 
-// switch (browser) {
-//     case 'Edge':
-//       alert( "You've got the Edge!" );
-//       break;
-  
-//     case 'Chrome':
-//     case 'Firefox':
-//     case 'Safari':
-//     case 'Opera':
-//       alert( 'Okay we support these browsers too' );
-//       break;
-  
-//     default:
-//       alert( 'We hope that this page looks ok!' );
-//   } 
+//TODO: rewrote if with switch construction 
 
-// ! works 
-// const browser = "a"
+const number = prompt("Введите число между 0 и 3");
 
-// if (browser === "Edge") {
-//     alert("You've got the Edge!")
-// } else if (browser === "Chrome") {
-//     alert("Okay we support these browsers too")
-// } else if (browser === "Firefox") {
-//     alert("Okay we support these browsers too")
-// } else if (browser === "Opera") {
-//     alert("Okay we support these browsers too")
-// } else {
-//     alert("We hope that this page looks ok!")
+// if (number === 0) {
+//   alert('Вы ввели число 0');
 // }
 
-//* 2-nd variant
-//! works 
-const browser = "a"
-if (browser === "Edge") {
-    alert("You've got the Edge!")
-} else if (browser === "Chrome" || browser === "Firefox" || browser === "Opera") {
-    alert("Okay we support these browsers too") 
-} else {
-    alert("We hope that this page looks ok!")
-}
+// if (number === 1) {
+//   alert('Вы ввели число 1');
+// }
 
+// if (number === 2 || number === 3) {
+//   alert('Вы ввели число 2, а может и 3');
+// }
+// !works
+switch (number) {
+    case "0":
+        alert("Вы ввели число 0");
+        break;
+    case "1":
+        alert("Вы ввели число 1");
+        break;
+    case "2": 
+    case "3":
+        alert("Вы ввели число 2, а может и 3");
+    // default:
+    //     break;
+}
