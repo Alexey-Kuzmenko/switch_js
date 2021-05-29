70d1673625510bf60d22754bb4d3f168e12ebb36
Alexey Kuzmenko <alesha1103348@gmail.com>
2021-05-29 15:40:02 +0300

    added switch construction

---
+++ switch.js
@@ -0,0 +1,25 @@
+let newUser = prompt("What is your name?")
+
+// let a = 2 + 2
+// if (a === 4) {
+//     alert("4!")
+// }
+
+switch (newUser) {
+    case "Alesha":
+        alert("Hello " + newUser)
+        break;
+    case "Alex":
+        alert("Hello " + newUser)
+        break;
+    case "Lera":
+        alert("Hello " + newUser)
+        break;
+
+    default:
+        alert("error")
+        break;
+}
+
+
+
