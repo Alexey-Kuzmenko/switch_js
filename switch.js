9c85ce882d8777d0e2434910a6706fc5dd8a33d0
Alexey Kuzmenko <alesha1103348@gmail.com>
2021-05-29 18:00:10 +0300

    first exercise

--- switch.js
+++ switch.js
@@ -1,12 +1,7 @@
-let newUser = prompt("What is your name?")
-
-// let a = 2 + 2
-// if (a === 4) {
-//     alert("4!")
-// }
-
+//! example 
 // switch (newUser) {
 //     case "Alesha":
+//     case "Alex":
 //         alert("Hello " + newUser)
 //         break;
 //     case "Egor":
@@ -21,20 +16,47 @@ let newUser = prompt("What is your name?")
 //         break;
 // }
 
-switch (newUser) {
-    case "Alesha":
-    case "Alex":
-        alert("Hello " + newUser)
-        break;
-    case "Egor":
-        alert("Hello " + newUser)
-        break;
-    case "Lera":
-        alert("Hello " + newUser)
-        break;
+//* exercise 1
 
-    default:
-        alert("error")
-        break;
+//TODO: rewrote with if construction 
+// switch (browser) {
+//     case 'Edge':
+//       alert( "You've got the Edge!" );
+//       break;
+  
+//     case 'Chrome':
+//     case 'Firefox':
+//     case 'Safari':
+//     case 'Opera':
+//       alert( 'Okay we support these browsers too' );
+//       break;
+  
+//     default:
+//       alert( 'We hope that this page looks ok!' );
+//   } 
+
+// ! works 
+// const browser = "a"
+
+// if (browser === "Edge") {
+//     alert("You've got the Edge!")
+// } else if (browser === "Chrome") {
+//     alert("Okay we support these browsers too")
+// } else if (browser === "Firefox") {
+//     alert("Okay we support these browsers too")
+// } else if (browser === "Opera") {
+//     alert("Okay we support these browsers too")
+// } else {
+//     alert("We hope that this page looks ok!")
+// }
+
+//* 2-nd variant
+const browser = "a"
+if (browser === "Edge") {
+    alert("You've got the Edge!")
+} else if (browser === "Chrome" || browser === "Firefox" || browser === "Opera") {
+    alert("Okay we support these browsers too") 
+} else {
+    alert("We hope that this page looks ok!")
 }
 
